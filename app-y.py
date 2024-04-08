a51617b4c6bb9745f421b874569b8c036f088828
Bibekmanandar <166355950+Bibekmanandar@users.noreply.github.com>
2024-04-08 14:42:48 +0545

    Update app-y.py

--- app-y.py
+++ app-y.py
@@ -8,7 +8,7 @@ def feature_y():
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
-                print(num, "is not a prime number")
+                print(num, "is not prime number")
                 print(i, "times", num // i, "is", num)
                 break
         else:
